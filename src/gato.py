17e1dd1258eac6eee569c450cc3fba30754b73d2
Gustavo Lagoa <gustavolagoa@MacBook-Pro-de-Gustavo.local>
2022-05-10 09:07:47 -0300

    Add existing file

--- src/gato.py
+++ src/gato.py
@@ -4,6 +4,6 @@ from pygame import init
 class Gato():
     def __init__(self,nome):
         self.nome = nome
-    
+        
     def fala_algo(self):
         return f"OI! Meu nome eh {self.nome}"
--- src/porgrama.py
+++ src/porgrama.py
@@ -1,4 +1,7 @@
+from gato import Gato
 from pessoa import Pessoa
 
 p1 = Pessoa('Joao', 30)
 p1.fala_algo()
+g1 = Gato('Roberto')
+g1.fala_algo()
